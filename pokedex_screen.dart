b826acae48d5b25c2aee0265a165e2cd3ee8bda2
Domiciano <domic.rincon@gmail.com>
2023-09-07 09:36:32 -0500

    Add files via upload

--- pokedex_screen.dart
+++ pokedex_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
-
 class PokedexScreen extends StatelessWidget {
   const PokedexScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Pokedex'),
@@ -23,31 +21,31 @@ class PokedexScreen extends StatelessWidget {
                 color: Color.fromARGB(255, 230, 234, 255),
               ),
               child: Image.network(
-                  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png",
-                  width: 200,
-                  height: 200,
-                  fit: BoxFit.fill,
-                )),
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/1200px-Pok%C3%A9_Ball_icon.svg.png",
+                width: 200,
+                height: 200,
+                fit: BoxFit.fill,
+              )),
           const SizedBox(height: 20),
           Text(
-              "Charizard",
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
+            "Charizard",
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
             ),
+          ),
           const SizedBox(height: 10),
           PokemonTypeTag(type: "Fuego"),
           const SizedBox(height: 20),
           Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                PokemonStat(statName: 'Attack', value: 100),
-                PokemonStat(statName: 'Defense', value: 60),
-                PokemonStat(statName: 'Speed', value: 50),
-              ],
-            ),
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              PokemonStat(statName: 'Attack', value: 100),
+              PokemonStat(statName: 'Defense', value: 60),
+              PokemonStat(statName: 'Speed', value: 50),
+            ],
+          ),
           Expanded(child: Container()),
           const SizedBox(height: 20),
           Padding(
@@ -56,9 +54,7 @@ class PokedexScreen extends StatelessWidget {
               children: [
                 Expanded(
                   child: TextFormField(
-                    onChanged: (value) {
-                      
-                    },
+                    onChanged: (value) {},
                     decoration: const InputDecoration(
                       hintText: 'Enter Pokémon name or number',
                     ),
@@ -66,9 +62,7 @@ class PokedexScreen extends StatelessWidget {
                 ),
                 const SizedBox(width: 10),
                 ElevatedButton(
-                  onPressed: () {
-                    
-                  },
+                  onPressed: () {},
                   child: const Text('Search'),
                 ),
               ],
